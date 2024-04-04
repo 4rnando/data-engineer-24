12fee9eb5a0f2846dd1325a2b5d217c84ecc161b
Fernando M <51896140+4rnando@users.noreply.github.com>
2024-04-04 10:10:10 -0600

    FM Response_Ex1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -20,3 +20,27 @@ ORDER BY
 	Location,
 	Year,
 	Month;
+
+
+
+---- Curiosity led me to see the highest amount of refunds in the most recent year.
+---- Grouped by year and highest total amount of refunds
+
+SELECT 
+    o.locationId AS Location,
+    YEAR(CAST(t.date AS DATE)) AS Year,
+    MONTH(CAST(t.date AS DATE)) AS Month,
+    ROUND(SUM(o.salesAmount), 0) AS TotalRefunded
+FROM
+    dbo.Transactions t
+INNER JOIN
+    dbo.Order_Items o ON t.Id = o.Id
+WHERE
+    t.type = 'refund'
+GROUP BY
+    o.locationId,
+    YEAR(CAST(t.date AS DATE)),
+    MONTH(CAST(t.date AS DATE))
+ORDER BY
+    Year DESC,
+    TotalRefunded DESC;
