eb9dc0d658d136f194bb31f9f85471ccf71cdf73
Fernando M <51896140+4rnando@users.noreply.github.com>
2024-04-04 09:50:43 -0600

    FM Response_Ex1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -1,2 +1,22 @@
--- write your SQL here
-select 1;
+-- Quered in MS SQL Server
+-- Exercise 1
+-- Given the data sources transactions and order items, write and submit a SQL query that identifies and groups the total amount refunded by location and month. 
+SELECT 
+	o.locationId AS Location,
+	YEAR(CAST(t.date AS DATE)) AS Year,
+	MONTH(CAST(t.date AS DATE)) AS Month,
+	ROUND(SUM(o.salesAmount), 0) AS TotalRefunded
+FROM
+	Transactions t
+INNER JOIN
+	Order_Items o ON t.Id = o.Id
+WHERE
+	t.type = 'refund'
+GROUP BY
+	o.locationId,
+	YEAR(CAST(t.date AS DATE)),
+	MONTH(CAST(t.date AS DATE))
+ORDER BY
+	Location,
+	Year,
+	Month;
